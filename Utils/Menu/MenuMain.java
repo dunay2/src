e4dae5dad4ec9c2c7af2aa95084a41428c4717a9
dunay2 <34416258+dunay2@users.noreply.github.com>
2018-04-29 23:09:37 +0100

    Version 0.9 listas cambio a mi arbol

--- Utils/Menu/MenuMain.java
+++ Utils/Menu/MenuMain.java
@@ -37,7 +37,7 @@ public class MenuMain extends MenuBase {
 //Creamos el nodo raíz mnuMain
         //Menu principal
         entries.add(new MenuStruct("mnuTransaction", "Realizar una Transacción"));
-        entries.add(new MenuStruct("mnuClients", "Gestión de Clientes"));
+        entries.add(new MenuStruct("mnuClient", "Gestión de Clientes"));
         entries.add(new MenuStruct("mnuStock", "Gestión de Stock"));
         entries.add(new MenuStruct("mnuEmployee", "Gestión de Empleados"));
         entries.add(new MenuStruct("", "Listar Facturas"));
@@ -47,17 +47,20 @@ public class MenuMain extends MenuBase {
         convertToChildNode(parentMnuName, entries);
         addTransactionMnu();
         addClientMnu();
+        addStockMnu();
     }
-    
-    
+
     private static void addTransactionMnu() {
         ArrayList<MenuStruct> entries = new ArrayList();
 
+        //menú principal
         MenuSale.transactionEntries("mnuTransaction").forEach((menuStruct) -> {
             entries.add(menuStruct);
         });
         convertToChildNode("mnuTransaction", entries);
         entries.clear();
+        
+        //Agregar items al carrito
         MenuSale.addItemToCartEntries("mnuAddItemToCart").forEach((menuStruct) -> {
             entries.add(menuStruct);
         });
@@ -67,11 +70,30 @@ public class MenuMain extends MenuBase {
 //            entries.add(menuStruct);
 //        });
 //    convertToChildNode("mnuBuying", entries);
-        MenuSale.paymentTypeEntries("mnuBuying").forEach((menuStruct) -> {
+
+    //Obtener id cliente
+        MenuSale.getClientIdEntries("mnuBuying").forEach((menuStruct) -> {
             entries.add(menuStruct);
         });
         convertToChildNode("mnuBuying", entries);
         entries.clear();
+        
+        //obtener datos de cliente
+        MenuSale.addClientEntriesNoID("mnuGetClientId").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuBuying", entries);
+        entries.clear();
+
+
+//        MenuSale.paymentTypeEntries("mnuBuying").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuBuying", entries);
+//        entries.clear();
+        
+        
+        
 
 //         MenuSale.getItemCode("mnuAddItemToCart").forEach((menuStruct) -> {
 //            entries.add(menuStruct);
@@ -79,32 +101,154 @@ public class MenuMain extends MenuBase {
 //        convertToChildNode("mnuAddItemToCart", entries);
 //        entries.clear();
     }
- 
 
     private static void addClientMnu() {
         ArrayList<MenuStruct> entries = new ArrayList();
 
-        MenuClient.addClientEntries("mnuClients").forEach((menuStruct) -> {
+        MenuClient.clientEntries("mnuClient").forEach((menuStruct) -> {
             entries.add(menuStruct);
         });
-        convertToChildNode("mnuTransaction", entries);
-        entries.clear();
-        
-        MenuSale.addItemToCartEntries("mnuAddItemToCart").forEach((menuStruct) -> {
-            entries.add(menuStruct);
-        });
-        convertToChildNode("mnuAddItemToCart", entries);
-        entries.clear();
-//        MenuSale.createFinanceMessage("mnuTransaction").forEach((menuStruct) -> {
-//            entries.add(menuStruct);
-//        });
-//    convertToChildNode("mnuBuying", entries);
-        MenuSale.paymentTypeEntries("mnuBuying").forEach((menuStruct) -> {
-            entries.add(menuStruct);
-        });
-        convertToChildNode("mnuBuying", entries);
+        convertToChildNode("mnuClient", entries);
         entries.clear();
 
+        //Propósito: Agregar clientes
+        //Padre: mnuClient
+        //Menú mnuAddClient
+        MenuClient.addClientEntries("mnuAddClient").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuAddClient", entries);
+        entries.clear();
+
+        //Propósito: Editar clientes
+        //Padre: mnuClient
+        //Menú mnuEditClient
+        MenuClient.editClientEntries("mnuEditClient").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuEditClient", entries);
+        entries.clear();
+
+        //Propósito: Deshabilitar clientes
+        //Padre: mnuClient
+        //Menú mnuDeleteClient
+        MenuClient.deleteClientEntries("mnuDeleteClient").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuDeleteClient", entries);
+        entries.clear();
+        //Propósito: Buscar clientes
+        //Padre: mnuClient
+        //Menú mnuSearchClient
+
+        MenuClient.searchClientEntries("mnuSearchClient").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuSearchClient", entries);
+        entries.clear();
+
+//
+//    //Propósito: Agregar entradas al menú para solicitar entrada de codigo cliente
+//    static protected ArrayList<MenuStruct> getClientIdEntries(String parentMnuName) {
+//        //19. mnuGetClientId 
+//
+//        ArrayList<MenuStruct> entries = new ArrayList();
+//        entries.add(new MenuStruct("", "Introduzca DNI del cliente"));
+//
+//        convertToChildNode("", entries);
+//        return entries;
+//    }
+//        MenuSale.addItemToCartEntries("mnuAddItemToCart").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuAddItemToCart", entries);
+//        entries.clear();
+////        MenuSale.createFinanceMessage("mnuTransaction").forEach((menuStruct) -> {
+////            entries.add(menuStruct);
+////        });
+////    convertToChildNode("mnuBuying", entries);
+//        MenuSale.paymentTypeEntries("mnuBuying").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuBuying", entries);
+//        entries.clear();
+//         MenuSale.getItemCode("mnuAddItemToCart").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuAddItemToCart", entries);
+//        entries.clear();
+    }
+
+    private static void addStockMnu() {
+        ArrayList<MenuStruct> entries = new ArrayList();
+//Menú principal de la rama
+        MenuStock.stockEntries("mnuStock").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuStock", entries);
+        entries.clear();
+
+        //Propósito: Agregar 
+        //Padre: mnuStock
+        //Menú mnuAddItem
+        MenuStock.addItemEntries("mnuAddItem").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuAddItem", entries);
+        entries.clear();
+
+        //Propósito: Editar 
+        //Padre: mnuStock
+        //Menú mnuEditItem
+        MenuStock.editItemEntries("mnuEditItem").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuEditItem", entries);
+        entries.clear();
+
+        //Propósito: Deshabilitar 
+        //Padre: mnuStock
+        //Menú mnuDeleteItem
+        MenuStock.deleteItemEntries("mnuDeleteItem").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuDeleteItem", entries);
+        entries.clear();
+        //Propósito: Buscar 
+        //Padre: mnuClient
+        //Menú mnuSearchClient
+
+        MenuStock.searchItemEntries("mnuSearchItem").forEach((menuStruct) -> {
+            entries.add(menuStruct);
+        });
+        convertToChildNode("mnuSearchItem", entries);
+        entries.clear();
+
+//
+//    //Propósito: Agregar entradas al menú para solicitar entrada de codigo cliente
+//    static protected ArrayList<MenuStruct> getClientIdEntries(String parentMnuName) {
+//        //19. mnuGetClientId 
+//
+//        ArrayList<MenuStruct> entries = new ArrayList();
+//        entries.add(new MenuStruct("", "Introduzca DNI del cliente"));
+//
+//        convertToChildNode("", entries);
+//        return entries;
+//    }
+//        MenuSale.addItemToCartEntries("mnuAddItemToCart").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuAddItemToCart", entries);
+//        entries.clear();
+////        MenuSale.createFinanceMessage("mnuTransaction").forEach((menuStruct) -> {
+////            entries.add(menuStruct);
+////        });
+////    convertToChildNode("mnuBuying", entries);
+//        MenuSale.paymentTypeEntries("mnuBuying").forEach((menuStruct) -> {
+//            entries.add(menuStruct);
+//        });
+//        convertToChildNode("mnuBuying", entries);
+//        entries.clear();
 //         MenuSale.getItemCode("mnuAddItemToCart").forEach((menuStruct) -> {
 //            entries.add(menuStruct);
 //        });
