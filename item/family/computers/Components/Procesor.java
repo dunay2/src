d99f30ceaa77b0854b8b79b611262855b280bf53
dunay2 <34416258+dunay2@users.noreply.github.com>
2018-05-05 14:43:38 +0100

    Version 0.9 listas cambio a mi arbol

---
+++ item/family/computers/Components/Procesor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package item.family.computers.Components;
+
+import item.family.computers.Computer;
+
+/**
+ *
+ * @author ashh412
+ */
+public class Procesor extends Computer {
+
+   
+    public Procesor(String code) {
+        super(code);
+    }
+
+}
