73228ee5a73d606bad5c089b45db79c3b8a016c4
dunay2 <34416258+dunay2@users.noreply.github.com>
2018-05-05 10:20:17 +0100

    Version 0.9 listas cambio a mi arbol

---
+++ item/family/computers/Components/Keyboard.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package item.family.computers.Components;
+
+import item.Electrodomestic;
+
+/**
+ *
+ * @author ashh412
+ */
+public class Keyboard extends Electrodomestic {
+    
+    public Keyboard(String code) {
+        super(code);
+    }
+    
+}
